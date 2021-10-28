1de97e060502899ca276e3697a490fcfbd19f010
Jhonatan Becerra <jsbpdeveloper@gmail.com>
2021-10-27 22:28:16 -0500

    Sistema de registro conectado con BD, el login aun no funrciona

--- app.js
+++ app.js
@@ -27,36 +27,63 @@ app.use(session({
 
 require('./database/db');
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
+//Rutas
+    app.get('/', (req, res) => {
+        res.render('index');
+    })
 
-app.get('/login', (req, res) => {
-    res.render('login');
-})
+    app.get('/login', (req, res) => {
+        res.render('login');
+    })
 
-app.get('/register', (req, res) => {
-    res.render('register');
-})
+    app.get('/register', (req, res) => {
+        res.render('register');
+    })
 
 
-app.listen(3000, (req, res) => {
-    console.log('Servidor ejecutandose in http://localhost:3000');
-});
-
-//Register
+    
+    //Register
 app.post('/register', async (req, res) => {
     const user = req.body.user;
     const name = req.body.name;
     const rol = req.body.rol;
     const pass = req.body.pass;
     let passwordHaash = await bcryptjs.hash(pass, 8);
-    connection.query('INSERT INTO user SET ?', {user:user, name:name, rol:rol, pass:passwordHaash}, async (err, result) => {
+    connection.query('INSERT INTO user SET ?', {user:user, name:name, rol:rol, pass:passwordHaash}, async (error, results) => {
         if(error){
-            console.log('Error');
+            console.log(error);
         }else{
-            res.send('User registrado')
-        }
+            res.render('register',{
+                alert: true,
+                alertTitle: "Usuario Registrado",
+                alertMessage: "proceso exitoso",
+                alertIcon: 'success',
+                showConfirmButton: false,
+                timer:1500,
+                ruta:''
+
+            })
+         }
     }) 
 })
+    //Auth
 
+app.post('/auth', async (req, res) => {
+    const user = req.body.user;
+    const pass = req.body.pass;
+    let passwordHaash = await bcryptjs.hash(pass, 8);
+    if(user && pass){
+        connection.query('SELECT * FROM users WHERE user = ?', [user], async (error, results)=>{
+            if(results.length == 0 || !(await bcryptjs.compare(pass, results[0].pass))){
+                res.send('User y/o Pass incorrecto');
+            }else{
+                res.send('Bienvenido');
+            }
+        })
+    }
+
+})
+
+app.listen(3000, (req, res) => {
+    console.log('Servidor ejecutandose in http://localhost:3000');
+});
